5b6739339211b388e7791be877019e7edf195c23
kolkko <8045612+kolkko@users.noreply.github.com>
2020-12-22 15:56:59 +0000

    Rewrite time format function

--- src/format.cpp
+++ src/format.cpp
@@ -1,35 +1,18 @@
 #include <string>
-#include <bits/stdc++.h>
+#include <iomanip>
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-std::string Format::ElapsedTime(long times){
-    std::string timeformat, hourstr, minstr, secstr;
-    long hours, minutes, seconds;
-
-    //Calculate hours minutes and seconds from Uptime in seconds
-    hours = floor(times / 3600);
-    minutes = floor((times%3600)/60);
-    seconds = times%60;
-    
-    //Format times for display
-    if(hours >= 1 && hours < 10) {hourstr = "0" + std::to_string(hours);}
-    else if(hours < 1 ) {hourstr = "00";}
-    else {hourstr = std::to_string(hours);}
-
-    if(minutes >= 1 && minutes < 10) {minstr = "0" + std::to_string(minutes);}
-    else if(minutes < 1 ) {minstr = "00";}
-    else {minstr = std::to_string(minutes);}
-    
-    if(seconds >= 1 && seconds < 10) {secstr = "0" + std::to_string(seconds);}
-    else if(seconds < 1 ) {secstr = "00";}
-    else {secstr = std::to_string(seconds);}
-
-    timeformat = hourstr + ":" + minstr + ":" + secstr;
-
-return timeformat;}
+string Format::ElapsedTime(long seconds) {
+  long h = seconds / 3600;
+  long m = (seconds % 3600) / 60;
+  long s = (seconds % 3600 % 60);
+  std::ostringstream elapsedTime;
+  elapsedTime << std::setw(2) << std::setfill('0') << h 
+              << ":" << std::setw(2) << std::setfill('0') << m
+              << ":" << std::setw(2) << std::setfill('0') << s;
+  return elapsedTime.str();
+ }
